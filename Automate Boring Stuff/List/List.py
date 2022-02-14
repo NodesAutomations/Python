a01dda4797e178c52ee99f383b31534b1658626b
Vivek <vivek@nodesautomations.com>
2022-02-14 09:13:20 +0530

    List Sample Code Added

---
+++ Automate Boring Stuff/List/List.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+
+# regular List
+names=["vivek","Deven","Dhruv","Yogesh"]
+# print(names[1])
+
+#multy Index List
+names=[["Kaledin","Dalenar"],["Navani","Yashnah"]]
+print(names[0])
+print(names[0][1])

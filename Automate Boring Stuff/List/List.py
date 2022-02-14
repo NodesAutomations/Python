50fa1dac78fe3e9f9ef36c4fae36b9573d2f2b8b
Vivek <vivek@nodesautomations.com>
2022-02-14 09:27:52 +0530

    List Range

--- Automate Boring Stuff/List/List.py
+++ Automate Boring Stuff/List/List.py
@@ -2,7 +2,15 @@ from unicodedata import name
 
 # regular List
 names=["vivek","Deven","Dhruv","Yogesh"]
-# print(names[1])
+#positive index go from left to right start with 0
+print(names[1])
+#negative index go from right to left start with -1
+print(names[-1]) 
+#slices in list aka list range
+#list range include value from starting index and one less than end index
+print(names[1:3])
+print(names[1:-1])
+print(names[2:]) # Leving blank index will just use index to end of list
 
 #multy Index List
 names=[["Kaledin","Dalenar"],["Navani","Yashnah"]]
